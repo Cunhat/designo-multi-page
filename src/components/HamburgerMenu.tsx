453ee32d62de27dd51147cef1efc09e814ab5659
Tiago Miguel Cunha <tiago92cunha@gmail.com>
2022-12-15 15:39:44 +0000

    Add typography components

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -8,7 +8,10 @@ export interface ItemProps {
 const Item: React.FC<ItemProps> = ({ title, href }) => {
   return (
     <li>
-      <a className="text-white" href={href}>
+      <a
+        className="text-white font-regular text-2xl tracking-wide "
+        href={href}
+      >
         {title}
       </a>
     </li>
@@ -17,6 +20,13 @@ const Item: React.FC<ItemProps> = ({ title, href }) => {
 
 export const HamburgerMenu: React.FC<{}> = () => {
   const [open, setOpen] = useState(false);
+
+  const openMenu = () => {
+    const scroll = open;
+    setOpen(!scroll);
+    document.body.style.overflow = scroll ? "unset" : "hidden";
+  };
+
   return (
     <li className="flex items-center ml-auto md:hidden xl:hidden">
       <div>
@@ -25,14 +35,14 @@ export const HamburgerMenu: React.FC<{}> = () => {
           height="20"
           width="24"
           className="cursor-pointer"
-          onClick={() => setOpen(!open)}
+          onClick={() => openMenu()}
         />
         {open && (
           <div className="absolute top-[95px] left-0 w-full h-full bg-black bg-opacity-70">
             <ul className="py-12 px-6 flex flex-col gap-8 bg-black w-full">
-              <Item title="Teste" href="/" />
-              <Item title="Teste" href="/" />
-              <Item title="Teste" href="/" />
+              <Item title="OUR COMPANY" href="/" />
+              <Item title="LOCATIONS" href="/" />
+              <Item title="CONTACT" href="/" />
             </ul>
           </div>
         )}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        peach: "#E7816B",
+        lightPeach: "#FFAD9B",
+        darkGrey: "#333136",
+        lightGrey: "#f1f3f5",
+      },
+      fontFamily: {
+        medium: "JostMedium",
+        regular: "JostRegular",
+      },
+    },
+  },
+  plugins: [],
+};
